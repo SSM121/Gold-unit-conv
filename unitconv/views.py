cdb59d1d74aeec1a28e0a55a43b123921d854273
Spencer Kasteler <sdkasteler@gmail.com>
2020-11-10 12:04:29 -0700

    first commit. made 2 apps and added them to urls patterns.

---
+++ unitconv/views.py
@@ -0,0 +1,7 @@
+
+# Create your views here.
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello. welcome to unitconv")
